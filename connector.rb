62071f013a2b1f846d12db453e80706e3507479b
Kyle Tan <kyle@arist.co>
2025-01-22 21:30:00 -0800

    Connects to API successfully

---
+++ connector.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+{
+  title: 'Google AppSheet',
+
+  connection: {
+    fields: [
+      {
+        name: 'application_access_key',
+        label: 'Application Access Key',
+        control_type: 'password',
+        optional: false
+      },
+      {
+        name: 'app_id',
+        label: 'App ID',
+        optional: false
+      },
+      {
+        name: 'region',
+        label: 'Region',
+        control_type: 'subdomain',
+        url: 'appsheet.com',
+        hint: 'www or eu',
+        optional: false
+      },
+      {
+        name: 'test_table',
+        label: 'Test Table',
+        hint: 'Please enter a valid table name so the connector' \
+        ' can verify connectivity. The table must exist. For' \
+        ' continuity, choose the table that is most likely' \
+        ' to not change in name.',
+        optional: false
+      }
+    ],
+
+    authorization: {
+      type: 'basic_auth',
+
+      apply: lambda do |connection|
+        headers('ApplicationAccessKey': connection['application_access_key'])
+      end
+    },
+
+    base_uri: lambda do |connection|
+      "https://#{connection['region']}.appsheet.com/api/v2/apps/#{connection['app_id']}/"
+    end
+  },
+
+  test: lambda do |connection|
+    # This will return an empty row due to the selector, but it's a valid response
+    post("tables/#{connection['test_table']}/Action")
+      .payload(Action: 'Find', Properties: { Selector: 'TOP(X, 1)' })
+  end
+}
